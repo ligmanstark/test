41735005df02d0d01f96ced25915851fc7fce459
levis <miduwaystark@gmail.com>
2024-12-30 21:12:35 +0400

    first

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AppHeader from '../components/AppHeader/index.vue'
+import AppFooter from '../components/AppFooter/index.vue';
+</script>
+
+<template>
+<div>
+    <AppHeader/>
+    <slot></slot>
+
+    <AppFooter/>
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Welcome</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio deleniti quos perferendis tempora, mollitia recusandae animi earum, quaerat atque, quam quae incidunt at aliquid odio aut praesentium blanditiis officia aperiam.</p>
+    </div>
+</template>
