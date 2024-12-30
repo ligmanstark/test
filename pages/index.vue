981d3f0a12338cdc58a135bfd7e23fb898170b8f
levis <miduwaystark@gmail.com>
2024-12-30 21:31:35 +0400

    add content

--- pages/index.vue
+++ pages/index.vue
@@ -3,4 +3,5 @@
         <h2>Welcome</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio deleniti quos perferendis tempora, mollitia recusandae animi earum, quaerat atque, quam quae incidunt at aliquid odio aut praesentium blanditiis officia aperiam.</p>
     </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deserunt officia porro aperiam saepe nisi possimus beatae consectetur natus optio rerum, dolore quia architecto quo nemo, nihil quibusdam? Excepturi, suscipit.</p>
 </template>
