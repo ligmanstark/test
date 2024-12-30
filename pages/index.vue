5c733746247398f9c61b7af68d02d2a8aeb21194
levis <miduwaystark@gmail.com>
2024-12-30 21:52:46 +0400

    test

--- pages/index.vue
+++ pages/index.vue
@@ -4,4 +4,6 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio deleniti quos perferendis tempora, mollitia recusandae animi earum, quaerat atque, quam quae incidunt at aliquid odio aut praesentium blanditiis officia aperiam.</p>
     </div>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deserunt officia porro aperiam saepe nisi possimus beatae consectetur natus optio rerum, dolore quia architecto quo nemo, nihil quibusdam? Excepturi, suscipit.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus iure sit accusantium, quos magni id amet eum asperiores. Ducimus provident recusandae incidunt iusto atque iure nemo ipsam, at cupiditate facilis.</p>
+<button></button>
 </template>
